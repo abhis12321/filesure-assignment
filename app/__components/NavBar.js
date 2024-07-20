2558281a65022307fd6176788cb675ea15532c83
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-20 17:03:09 +0530

    NavBar is completed

--- app/__components/NavBar.js
+++ app/__components/NavBar.js
@@ -1,9 +1,34 @@
+import { faMobile , faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className=''>
-      navbar
+    <div className='flex items-center justify-between px-6 h-[4.5rem] shadow-[0_0_1px_gray] font-medium'>
+        <div className="flex">
+            <Image src={'/logo.png'} alt='logo' height={50} width={100} className='h-[4rem] w-auto' />
+        </div>
+
+        <div className="flex gap-8">
+            <Link href={'/'} className='hover:scale-110 hover:text-indigo-600'>Services</Link>
+            <Link href={'/'} className='hover:scale-110 hover:text-indigo-600'>Media</Link>
+            <Link href={'/'} className='hover:scale-110 hover:text-indigo-600'>Cases</Link>
+            <Link href={'/'} className='hover:scale-110 hover:text-indigo-600'>FAQ</Link>
+            <Link href={'/'} className='hover:scale-110 hover:text-indigo-600'>Contacts</Link>
+        </div>
+
+        <div className="flex gap-8 text-lime-600">
+            <div className="hover:text-lime-400 cursor-default flex gap-3 items-center justify-center">
+                <FontAwesomeIcon size='xs' icon={faMobile} className='h-[1.5rem]' />
+                <div className="">+91 0000000000</div>
+            </div>
+            <div className="hover:text-lime-400 cursor-default flex gap-3 items-center justify-center">
+            <FontAwesomeIcon size='xs' icon={faEnvelope} className='h-[1.5rem]' />
+                <div className="">example@gmail.com</div>
+            </div>
+        </div>
     </div>
   )
 }
