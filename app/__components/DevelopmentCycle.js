0ae9a2170c6d243a05c26aa4c989fd64c885ce07
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-21 00:29:55 +0530

    Development course info page is created

---
+++ app/__components/DevelopmentCycle.js
@@ -0,0 +1,39 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import React from 'react'
+
+export default function DevelopmentCycle() {
+    return (
+        <div className='py-10'>
+            <div className="w-full [background:url('/planet.png')] bg-contain h-[140px] border-y-[1px] border-gray-300" />
+            <div className="flex justify-around py-10">
+                <div className="flex flex-col gap-4 max-w-[500px]">
+                    <h2 className="text-3xl font-bold font-sans tracking-normal">Full development cycle</h2>
+                    <p className="text-[10px] font-semibold py-2">We use proved technologies</p>
+                    
+                    <h2 className="text-[1rem] font-bold font-sans tracking-normal text-gray-800">Web</h2>
+                    <div className="text-sm tracking-wide font-medium text-gray-700">
+                        PHP / Javascrip / Node.JS / Web Socket / Socket.io / Vue.js / Nuxt / MySQL / Laravel / Go lang / django / Python 
+                    </div>
+                    
+                    <h2 className="text-[1rem] font-bold font-sans tracking-normal text-gray-800">Mobile</h2>
+                    <div className="text-sm tracking-wide font-medium text-gray-700">
+                        Swift / Kotlin / Alamofire / Firebase / CoreData / Room / Realm / Coroutine / RxJava / RxSwift / Unit Test / Navigation 
+                    </div>
+                    
+                </div>
+
+                <div className="flex flex-col gap-4">
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>iOS development <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>Android development <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>Web development <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>UI/UX design <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>Testing <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>Launch <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                    <Link href={'/'} className='w-fit flex items-center justify-center hover:underline text-gray-950/65 font-medium hover:text-gray-950'>IT consulting <FontAwesomeIcon size='xs' icon={faArrowRight} className='h-[1rem] text-lime-600/50' /></Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import IntroPage from './__components/IntroPage'
+import DevelopmentCycle from './__components/DevelopmentCycle'
 
 export default function page() {
   return (
     <div className=''>
       <IntroPage />
+      <DevelopmentCycle />
     </div>
   )
 }
